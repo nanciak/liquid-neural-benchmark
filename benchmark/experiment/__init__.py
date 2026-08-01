3856f37e56e84d784ff2f6050866e905ade6b25d
nanciak <150557928+nanciak@users.noreply.github.com>
2026-08-01 11:52:50 +0300

    Update __init__.py

---
+++ benchmark/experiment/__init__.py
@@ -0,0 +1,13 @@
+"""Public experiment package."""
+
+from benchmark.experiment.runner import (
+    ExperimentResult,
+    ExperimentRunner,
+    RunnerConfig,
+)
+
+__all__ = [
+    "ExperimentResult",
+    "ExperimentRunner",
+    "RunnerConfig",
+]
